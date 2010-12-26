1c863e499305fef505d8bb89993d5595cfab225e
cynipe <cynipe@gmail.com>
2010-12-27 00:13:58 +0900

    maven-appassembler-pluginのexample

---
+++ src/main/java/net/cynipe/example/maven/appassembler/Main.java
@@ -0,0 +1,21 @@
+package net.cynipe.example.maven.appassembler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) throws IOException {
+        final InputStream stream = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("config.xml");
+        final Properties configs = new Properties();
+        configs.loadFromXML(stream);
+        LOG.info("message: {}", configs.getProperty("message"));
+    }
+}
